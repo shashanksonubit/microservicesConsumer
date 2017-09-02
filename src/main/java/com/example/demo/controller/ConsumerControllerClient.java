1e18f3cee8ab49e73b1d3b6c64fa006d7d7ba064
shashank <shashank.sonu@gmail.com>
2017-09-02 20:50:32 +0530

    commit controller

--- src/main/java/com/example/demo/controller/ConsumerControllerClient.java
+++ src/main/java/com/example/demo/controller/ConsumerControllerClient.java
@@ -7,7 +7,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
-
+/**
+ * 
+ * @author shashank
+ *
+ */
 public class ConsumerControllerClient {
 
 	public void getEmployee() throws RestClientException, IOException {
